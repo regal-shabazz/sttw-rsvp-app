0e89f08bb8fb8c475884d7024f31e4e6daf29c0e
Babatunde Oshin (Regal Shabazz) <orsheenxi@gmail.com>
2026-02-27 16:55:06 +0100

    Increase delay for showing hero element

--- js/intro.js
+++ js/intro.js
@@ -70,7 +70,7 @@ export function initIntro() {
           hero.style.visibility = "";
         }
         stage.classList.add("show-hero");
-      }, 1500);
+      }, 2500);
     };
 
     let revealed = false;
