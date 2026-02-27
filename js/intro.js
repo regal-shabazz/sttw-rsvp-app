8dc7d5ecb6739abd6c328d7a8b099f23486a0bee
Babatunde Oshin (Regal Shabazz) <orsheenxi@gmail.com>
2026-02-27 17:00:44 +0100

    Reduce delay for showing hero from 4000ms to 3500ms

--- js/intro.js
+++ js/intro.js
@@ -70,7 +70,7 @@ export function initIntro() {
           hero.style.visibility = "";
         }
         stage.classList.add("show-hero");
-      }, 4000);
+      }, 3500);
     };
 
     let revealed = false;
